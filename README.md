71a533ee62d1e1f0180667e4bc7da429d244f40d
sebastien-riou <sriou@nimp.co.uk>
2025-12-30 21:32:06 +0100

    package and CI

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # usbman
-Software controlled USB hub
+Software controlled USB hub.
+
+## local build
+This build a native package.
+
+````
+pipenv run ./package_usbman
+````
--- usbman/__main__.py
+++ usbman/__main__.py
@@ -1,33 +1,8 @@
 import os,argparse
 from pysatl import Utils
 import logging
-from clicom import serial_command_response
+from usbman import get_state, set_state
 
-def decode_result(res:str) -> int:
-    logging.debug(f'res = {res}')
-    if res == b'EFF\r\n':
-        raise RuntimeError(f'Device returned "{res}"')
-    if res.startswith(b'G'):
-        res = res[1:]
-    state = res[:2]
-    state = state.decode()
-    state = Utils.ba(state)
-    state = int.from_bytes(state,byteorder='little')
-    state &= 0x7F
-    logging.debug(f'state = {state}')
-    return state
-
-def get_state(device_path) -> int:
-    state = serial_command_response(device_path, send_str='GP\r')
-    return decode_result(state)
-
-def set_state(device_path, state: int) -> int:
-    state |= 0x80
-    state = Utils.hexstr(state.to_bytes(1,byteorder='little'))
-    command = f'SPpass    {state}FFFFFF\r'
-    logging.debug(f'set state command: {command}')
-    state = serial_command_response(device_path, send_str=command)
-    return decode_result(state)
 
 if __name__ == '__main__':
     scriptname = os.path.basename(__file__)
