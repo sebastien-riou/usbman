b3a6db6b837cc9e75ed0b52fd062b6254dec65e9
Sebastien Riou <sebastien.riou@pqshield.com>
2026-01-02 13:14:48 +0100

    lint

--- usbman/__init__.py
+++ usbman/__init__.py
@@ -1,9 +1,11 @@
-import os,argparse
-from pysatl import Utils
 import logging
+
+from pysatl import Utils
+
 from usbman.clicom import serial_command_response
 
-def decode_result(res:str) -> int:
+
+def decode_result(res: str) -> int:
     logging.debug(f'res = {res}')
     if res == b'EFF\r\n':
         raise RuntimeError(f'Device returned "{res}"')
@@ -12,20 +14,21 @@ def decode_result(res:str) -> int:
     state = res[:2]
     state = state.decode()
     state = Utils.ba(state)
-    state = int.from_bytes(state,byteorder='little')
+    state = int.from_bytes(state, byteorder='little')
     state &= 0x7F
     logging.debug(f'state = {state}')
     return state
+
 
 def get_state(device_path) -> int:
     state = serial_command_response(device_path, send_str='GP\r')
     return decode_result(state)
 
+
 def set_state(device_path, state: int) -> int:
     state |= 0x80
-    state = Utils.hexstr(state.to_bytes(1,byteorder='little'))
+    state = Utils.hexstr(state.to_bytes(1, byteorder='little'))
     command = f'SPpass    {state}FFFFFF\r'
     logging.debug(f'set state command: {command}')
     state = serial_command_response(device_path, send_str=command)
     return decode_result(state)
-
--- usbman/__main__.py
+++ usbman/__main__.py
@@ -1,5 +1,5 @@
-import os,argparse
-from pysatl import Utils
+import os
+import argparse
 import logging
 from usbman import get_state, set_state
 
@@ -9,9 +9,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(scriptname)
     levels = ('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')
     parser.add_argument('--log-level', default='INFO', choices=levels)
-    parser.add_argument(
-        '--device-path', default='/dev/ttyUSB0', help='Path to the serial device', type=str
-    )
+    parser.add_argument('--device-path', default='/dev/ttyUSB0', help='Path to the serial device', type=str)
 
     parser.add_argument('--on', default=[], help='turn channel(s) on', nargs='+', type=int)
     parser.add_argument('--off', default=[], help='turn channel(s) off', nargs='+', type=int)
@@ -22,7 +20,7 @@ if __name__ == '__main__':
     logformat = '%(asctime)s.%(msecs)03d %(levelname)s:\t%(message)s'
     logdatefmt = '%Y-%m-%d %H:%M:%S'
     logging.basicConfig(level=args.log_level, format=logformat, datefmt=logdatefmt)
-    
+
     logging.debug(f'args = {args}')
     set_on = set(args.on)
     set_off = set(args.off)
@@ -30,7 +28,7 @@ if __name__ == '__main__':
     if conflicts:
         logging.error(f'ON and OFF arguments are conflicting for channels {conflicts}')
         exit(-1)
-    
+
     org_state = get_state(device_path)
     state = org_state
     logging.debug(f'org_state = {org_state:#02x}, state = {state:#02x}')
@@ -40,10 +38,10 @@ if __name__ == '__main__':
         logging.debug(f'ON = {set_on}, state = {state:#02x}')
     if set_off:
         for i in set_off:
-            state &= ~(1 << (i-1))
+            state &= ~(1 << (i - 1))
     logging.debug(f'org_state = {org_state:#02x}, state = {state:#02x}')
     if org_state != state:
-        final_state = set_state(device_path,state)
+        final_state = set_state(device_path, state)
         logging.debug(f'final_state = {final_state:#02x}')
         current_state = final_state
     else:
@@ -51,8 +49,8 @@ if __name__ == '__main__':
     if 0 == current_state:
         print('All off')
     else:
-        print('On: ',end='')
-        for i in range(1,8):
-            if current_state & (1<<(i-1)):
-                print(f'{i} ',end='')
+        print('On: ', end='')
+        for i in range(1, 8):
+            if current_state & (1 << (i - 1)):
+                print(f'{i} ', end='')
         print('')
